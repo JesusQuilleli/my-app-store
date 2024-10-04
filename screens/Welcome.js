e174a642e055325450a3ba00bb1481d3bef1ff5b
Jesus <jqcelis092@gmail.com>
2024-10-04 17:19:09 -0400

    Pantalla de Bienvenida Lista

---
+++ screens/Welcome.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react'
+import { Text, StyleSheet, View, Pressable, Image} from 'react-native'
+
+//COMPONENTE DE BIENVENIDA INICIO DE APLICACIÓN
+
+const Welcome = ({navigation}) => {
+
+   const [isPressed, setIsPressed] = useState(false); //ESTADO PARA SABER CUANDO ESTA PRESIONADO EL BOTON Y CUANDO NO
+
+  return (
+   <View style={styles.container}>
+      <Image source={require('../assets/resources/imagenPrueba.jpg')} style={styles.imagen}/>
+         <Text style={styles.titulo}>Inversiones{' '}<Text style ={styles.tituloBold}>Margarita</Text></Text>
+
+      <Pressable
+      onPressIn={() => setIsPressed(true)}
+      onPressOut={() => setIsPressed(false)}
+      onPress={() => navigation.navigate('Login')}
+      style={isPressed ? styles.btnPressed : styles.btnStarted}
+      >
+         <Text style={isPressed ? styles.btnPressedText : styles.btnStartedText}>
+            Vamos
+         </Text>
+      </Pressable>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+   //ESTILOS AQUI
+   container:{
+      backgroundColor: '#fff',
+      flex: 1,
+      justifyContent:'center',
+      alignItems:'center'
+   },
+   imagen:{
+      width: 250,
+      height: 250,
+      borderRadius: 50,
+      borderColor:'#fff',
+      marginBottom: 60
+   },
+   titulo:{
+      fontSize: 28,
+      fontWeight:'400'
+   },
+   tituloBold:{
+      fontWeight:'700',
+      color:'#fee03e'
+   },
+   btnStarted: {
+      padding: 15,
+      borderColor: '#fee03e',
+      borderWidth: 2,
+      borderRadius: 20,
+      marginTop: 100,
+      backgroundColor: '#fff'
+   },
+   btnPressed: {
+      marginTop: 100,
+      backgroundColor: '#fee03e',
+      padding: 15,
+      borderRadius: 20,
+      borderColor: '#000',
+      borderWidth: 2,
+    },
+   btnStartedText:{
+      color: '#000',
+      fontSize: 24,
+      fontWeight:'700'
+   },
+   btnPressedText: {
+      color: '#000',
+      fontSize: 24,
+      fontWeight: '700',
+    },
+})
+
+
+export default Welcome
